ef97bcc7a57e86167ca4cbefae5e34b6378975a1
tkane2000 <tkthompson@gmail.com>
2016-01-04 12:06:30 -0500

    rm TODO comment

--- public/js/main.js
+++ public/js/main.js
@@ -25,28 +25,6 @@ var box = new Box();
 var counter = new CountView();
 var addColor = new AddColorView();
 
-/*
-  TODO:
-    X: put/post request
-    debug create method
-    form validation
-     X: - defaults on Color model
-     - input valid hex (use regex)
-    security for put/post requests?
-    create appView
-      use it to append other views to body tag or content el
-    add color picker
-    add in router:
-       - hp
-       - directions
-       - etc
-    add in templates (nunjucks?)
-    X: break out views into separate files
-*/
-
-
-
-
 
 
 
